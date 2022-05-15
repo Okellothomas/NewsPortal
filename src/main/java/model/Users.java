2682becfda8c79b1fadd67d85598d40a01a4f3d1
Okellothomas <thomasolal33@gmail.com>
2022-05-15 15:38:09 +0300

    Update the models

--- src/main/java/model/Users.java
+++ src/main/java/model/Users.java
@@ -1,4 +1,12 @@
 package model;
 
 public class Users {
+
+    private String name;
+    private String role;
+    private String position;
+    private int departmentId;
+    private int id;
+
+
 }
