7feabdcd77538b36e39688d077faa4ea666b76bd
Okellothomas <thomasolal33@gmail.com>
2022-05-15 15:58:11 +0300

    Add the variables, setters and getter and the hashcode for department class

--- src/main/java/model/Departments.java
+++ src/main/java/model/Departments.java
@@ -1,4 +1,65 @@
 package model;
 
-public class Departments {
+import java.util.Objects;
+
+public class Departments  {
+    private String name;
+    private String description;
+    private  int number_of_employees;
+    int id;
+
+    public Departments(String name, String description, int number_of_employees) {
+        this.name = name;
+        this.description = description;
+        this.number_of_employees = number_of_employees;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getNumber_of_employees() {
+        return number_of_employees;
+    }
+
+    public void setNumber_of_employees(int number_of_employees) {
+        this.number_of_employees = number_of_employees;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Departments that = (Departments) o;
+        return number_of_employees == that.number_of_employees &&
+                id == that.id &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, number_of_employees, id);
+    }
 }
