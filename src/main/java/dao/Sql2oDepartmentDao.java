9c2479e6c48d3ba786c0d9f4f2e3d81545cb881d
Okellothomas <thomasolal33@gmail.com>
2022-05-16 08:54:21 +0300

    add the add user query logic

--- src/main/java/dao/Sql2oDepartmentDao.java
+++ src/main/java/dao/Sql2oDepartmentDao.java
@@ -1,4 +1,65 @@
 package dao;
 
-public class Sql2oDepartmentDao {
+import model.Departments;
+import model.News;
+import model.Users;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oDepartmentDao implements DepartmentDao {
+
+    private Sql2o sql2o;
+
+    public Sql2oDepartmentDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(Departments department) {
+
+        String query = "INSERT INTO mentors (name,description,number_of_employees) values(:name, :description, :number_of_employees)";
+
+        try (Connection conn = sql2o.open()) {
+            int id = (int) conn.createQuery(query, true)
+                    .bind(department)
+                    .executeUpdate()
+                    .getKey();
+            department.setId(id);
+        }catch (Sql2oException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void addUserToDepartment(Users user, Departments department) {
+
+    }
+
+    @Override
+    public List<Departments> getAll() {
+        return null;
+    }
+
+    @Override
+    public Departments findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Users> getAllUsersInDepartment(int department_id) {
+        return null;
+    }
+
+    @Override
+    public List<News> getDepartmentNews(int id) {
+        return null;
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
--- src/main/java/dao/Sql2oNewsDao.java
+++ src/main/java/dao/Sql2oNewsDao.java
@@ -1,4 +1,48 @@
 package dao;
 
-public class Sql2oNewsDao {
+import model.News;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oNewsDao implements NewsDao{
+
+    private Sql2o sql2o;
+
+    public Sql2oNewsDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void addNews(News news) {
+
+        String query = "INSERT INTO mentors (tittle,description,departmental_title) values(:tittle, :description, :departmental_title)";
+
+        try (Connection conn = sql2o.open()) {
+            int id = (int) conn.createQuery(query, true)
+                    .bind(news)
+                    .executeUpdate()
+                    .getKey();
+            news.setId(id);
+        }catch (Sql2oException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<News> getAll() {
+        return null;
+    }
+
+    @Override
+    public News findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
